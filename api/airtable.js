c55957eee8f401d93703471b41d664714fefa595
MTtahiri <72506249+MTtahiri@users.noreply.github.com>
2025-09-09 22:05:35 +0200

    Update airtable.js

--- api/airtable.js
+++ api/airtable.js
@@ -1,10 +1,22 @@
+// api/airtable.js
+const fetch = require('node-fetch');
+
 module.exports = async (req, res) => {
-  return res.status(200).json({
-    message: "API fonctionne",
-    env: {
-      hasApiKey: !!process.env.AIRTABLE_API_KEY,
-      baseId: process.env.AIRTABLE_BASE_ID,
-      tableName: process.env.AIRTABLE_TABLE_NAME
-    }
-  });
+  if (req.method !== 'GET') {
+    return res.status(405).json({ error: 'Method not allowed' });
+  }
+
+  try {
+    const response = await fetch(`https://api.airtable.com/v0/appNwg9iP8ub0cDCn/Candidats`, {
+      headers: {
+        'Authorization': 'Bearer pathXG85b7P96CnjE.8fe40141abdb308305bfb43100855b821751c55e38edf0680bce3a9d19243235'
+      }
+    });
+
+    const data = await response.json();
+    return res.json(data);
+
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
 };
